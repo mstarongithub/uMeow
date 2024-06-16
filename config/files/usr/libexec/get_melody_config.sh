2cbc733a97a3a8f40464648d40954877b10bf626
mStar <me@mstar.dev>
2024-06-16 14:48:03 +0200

    Moved flatpak setup to yafti
    - Yafti is now responsible for flatpak setup stuff
    - Added a note to please not remove the dotfiles git clone as configs
      are symlinks to it's contents

--- config/files/usr/libexec/get_melody_config.sh
+++ config/files/usr/libexec/get_melody_config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 
-echo "cloning config repo to $HOME/dotfiles"
-git clone https://gitlab.com/mstarongitlab/dotfiles.git $HOME/dotfiles
+echo "cloning config repo to $HOME/.dotfiles"
+git clone https://gitlab.com/mstarongitlab/dotfiles.git $HOME/.dotfiles
 
 echo "Running dotfiles setup script. Note: Old configs, if available, will be moved to $NAME-old"
-./$HOME/dotfiles/setup.sh
+./$HOME/.dotfiles/setup.sh
+
+echo "Please do not remove the .dotfiles directory as configs inserted are symlinks to content inside that directory for ease of updating"
